e399597b10a71cf9c936abe3ac80ae81df94da15
M Aghead Albakhe <aghyadalbalkhi@gmail.com>
2020-12-01 23:02:17 +0200

    some changing according to feedback

--- math_series/series.py
+++ math_series/series.py
@@ -1,3 +1,8 @@
+"""
+The  Fibonacci Function
+This Function is a sequence series of numbers where a number is the addition of the last two numbers, starting with 0, and 1.
+"""
+
 def fibonacci(n):
     if type(n) != int:
         return 'Plz Enter a Number'
@@ -11,6 +16,11 @@ def fibonacci(n):
         return fibonacci(n-1)+fibonacci(n-2)
 
 # print(fibonacci(0))
+
+"""
+The  lucas Function
+This lucas is a sequence series of numbers where a number is the addition of the last two numbers, starting with 2, and 1.
+"""
 
 def lucas(n):
     if type(n) != int:
@@ -26,6 +36,11 @@ def lucas(n):
 
 # print(lucas(0))
 
+"""
+The  Other_values Function
+This Other_values is a sequence series of numbers where a number is the addition of the last two numbers, starting with values that provide by user.
+"""
+
 def other_values(n,first_value,second_value):
     if type(n) != int:
         return 'Plz Enter a Number'
@@ -38,6 +53,10 @@ def other_values(n,first_value,second_value):
     else:
         return other_values(n-1,first_value,second_value)+other_values(n-2,first_value,second_value)
 
+"""
+The  sum_series Function
+sum_series Function check the base value and cotrol the call of funtion to determin which function should run
+"""
 
 def sum_series(n,first_value=0,second_value=1):
     if first_value ==0 and second_value ==1:
